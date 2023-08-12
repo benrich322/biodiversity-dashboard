aa9c88875e7a24c2c1415a5fc0ba8f9e67e47b94
Ben Richardson <benrichardson@bens-air.lan>
2023-08-12 11:34:32 -0500

    fetch JSON data

---
+++ static/js/app.js
@@ -0,0 +1,7 @@
+// 
+const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
+
+// Fetch the JSON data and console log it
+d3.json(url).then(function(data) {
+  console.log(data.samples);
+});
