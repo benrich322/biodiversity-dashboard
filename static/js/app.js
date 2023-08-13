85462c9ec545df45a82124920e11b1ef863093e8
Ben Richardson <benrichardson@Bens-MacBook-Air.local>
2023-08-12 20:04:22 -0500

    reset

--- static/js/app.js
+++ static/js/app.js
@@ -1,13 +1,8 @@
-// 
 const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
 
 // Fetch the JSON data and console log it
 d3.json(url).then(function(data) {
-  console.log(data.samples);
-});
+    console.log(data);
+  });
 
-// Initialize dashboard
-function init() {
-    // Use D3 to select the dropdown menu
-    let dropdownMenu = d3.select("#selDataset");
   
