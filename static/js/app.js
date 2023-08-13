5cc7ba4d026ffdbb3a77a7c5cb21510533e71a2c
Ben Richardson <benrichardson@Bens-MacBook-Air.local>
2023-08-12 21:22:16 -0500

    update formatting

--- static/js/app.js
+++ static/js/app.js
@@ -2,56 +2,44 @@ const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/
 
 // Fetch the JSON data and console log it
 d3.json(url).then(function(data) {
+
+    // console all data & sample data
     console.log('All Data',data);
     console.log('Sample Data',data.samples[0]);
 
-   let samples_data = data.samples[0]
-   let out_ids = samples_data.otu_ids
-   let sample_values = samples_data.sample_values
-   //let top_10_OTU_IDs = out_ids.slice(0, 10)
-   let top_10_Sample_Values = sample_values.slice(0, 10)
-   // Add 'OTU' in front
-   let top_10_OTU_IDs = out_ids.slice(0, 10).map(id => 'OTU ' + id.toString());
-   console.log('Top 10 OTU IDs',top_10_OTU_IDs)
-   console.log('Top 10 Sample Values',top_10_Sample_Values)
-   // Create Plotly plot
-   let plotData = [{
-    x: top_10_Sample_Values,
-    y: top_10_OTU_IDs,
-    type: 'bar',
-    orientation: 'h'
+    // defined variables
+    let samples_data = data.samples[0]
+    let out_ids = samples_data.otu_ids
+    let sample_values = samples_data.sample_values
+    // find first 10 sample values and otu ids
+    let top_10_sample_values = sample_values.slice(0, 10)
+    let top_10_otu_ids = out_ids.slice(0, 10).map(id => 'OTU ' + id.toString());
+
+    // conasole the first 10
+    console.log('Top 10 OTU IDs',top_10_otu_ids)
+    console.log('Top 10 Sample Values',top_10_sample_values)
+
+    // create Plotly plot
+    let plotData = [{
+        x: top_10_sample_values,
+        y: top_10_otu_ids,
+        type: 'bar',
+        orientation: 'h'
     }];
+    
+    // sort the data in ascending order
+    //plotData[0].x.sort((a, b) => a - b);
+    //plotData[0].y.sort((a, b) => a - b);
 
-  let layout = {
-    title: 'Top 10 OTU IDs vs Sample Values',
-    xaxis: {
-      title: 'Sample Values'
-    },
-    yaxis: {
-      title: 'OTU IDs'
-    }
-  };
+    let layout = {
+        title: 'Top 10 OTU IDs vs Sample Values',
+        xaxis: {
+            title: 'Sample Values'
+        },
+        yaxis: {
+            title: 'OTU IDs'
+        }
+    };
 
-  Plotly.newPlot("bar", plotData, layout);
+    Plotly.newPlot("bar", plotData, layout);
 });
-
-//barchart();
-
- //  let trace1 = {
-  //  x: books,
-   // y: timesRead,
-   // type: 'bar'
- // };
-  
- // let data = [trace1];
-  
- // let layout = {
- //   title: title
-//  };
-  
- // Plotly.newPlot("plot", data, layout);
-   //let left = names.slice(0, 2);
-
-   
-//});
-
