819fd57e50501b0da922011537c2a9cc4fe4e40e
Ben Richardson <benrichardson@bens-air.lan>
2023-08-12 23:24:27 -0500

    figured out better way for bar chart

--- static/js/app.js
+++ static/js/app.js
@@ -6,36 +6,30 @@ d3.json(url).then(function(data) {
     // console all data & sample data
     console.log('All Data',data);
     console.log('Sample Data',data.samples[0]);
+    
 
     // defined variables
-    let samples_data = data.samples[0]
-    let out_ids = samples_data.otu_ids
-    let sample_values = samples_data.sample_values
+    let samples_data = data.samples[0];
+    let otu_ids = samples_data.otu_ids;
+    let sample_values = samples_data.sample_values;
+    
+    console.log('OTU IDs',otu_ids);
+    console.log('Sample Values',sample_values);
+    // combine sample values and OTU IDs
+    let combined_data = sample_values.map((value, index) => ({
+            value: value,
+            otu_id: otu_ids[index],
+        }));
+    let sorted_data = combined_data.sort((a, b) => b.value - a.value);
+    console.log('Sorted Data',sorted_data)
 
-    // find first 10 sample values and otu ids
-    let top_10_sample_values = sample_values.slice(0, 10)
-    let top_10_otu_ids = out_ids.slice(0, 10).map(id => 'OTU ' + id.toString());
-
-    // conasole the first 10
-    console.log('Top 10 OTU IDs',top_10_otu_ids)
-    console.log('Top 10 Sample Values',top_10_sample_values)
-
-    // sort the data in ascending order
-    let sorted_indexes = top_10_sample_values
-    .map((_, index) => index)
-    .sort((a, b) => top_10_sample_values[a] - top_10_sample_values[b]);
-    console.log('Sorted Indexes',sorted_indexes)
-
-    let sorted_sample_values = sorted_indexes.map(index => top_10_sample_values[index]);
-    console.log('Sorted Sample Values',sorted_sample_values)
-
-    let sorted_otu_ids = sorted_indexes.map(index => top_10_otu_ids[index]);
-    console.log('Sorted OTU IDs',sorted_otu_ids)
+    let top_10_sorted_data = sorted_data.slice(0, 10);
+    console.log('Top 10 Data',top_10_sorted_data)
 
     // create Plotly plot
     let plotData = [{
-        x: sorted_sample_values,
-        y: sorted_otu_ids,
+        x: top_10_sorted_data.map(entry => entry.value), // Use the values from sortedData
+        y: top_10_sorted_data.map(entry => 'OTU ' + entry.otu_id.toString()), // Use the OTU IDs from sortedData
         type: 'bar',
         orientation: 'h',
     }];
