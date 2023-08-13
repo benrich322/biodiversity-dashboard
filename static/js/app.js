e8ff3dcad00fa6de757eddb6cc0ce98a19b5f50c
Ben Richardson <benrichardson@bens-air.lan>
2023-08-12 23:29:20 -0500

    cleaned up code

--- static/js/app.js
+++ static/js/app.js
@@ -1,20 +1,21 @@
 const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
 
-// Fetch the JSON data and console log it
+// fetch the JSON data and console log it
 d3.json(url).then(function(data) {
 
-    // console all data & sample data
     console.log('All Data',data);
-    console.log('Sample Data',data.samples[0]);
-    
 
-    // defined variables
     let samples_data = data.samples[0];
+    console.log('Sample Data',data.samples[0]);
+
+    // hold the array of OTU IDs extracted from the samples_data object    
     let otu_ids = samples_data.otu_ids;
-    let sample_values = samples_data.sample_values;
-    
     console.log('OTU IDs',otu_ids);
+
+    // hold the array of Sample Values extracted from the samples_data object
+    let sample_values = samples_data.sample_values;
     console.log('Sample Values',sample_values);
+
     // combine sample values and OTU IDs
     let combined_data = sample_values.map((value, index) => ({
             value: value,
